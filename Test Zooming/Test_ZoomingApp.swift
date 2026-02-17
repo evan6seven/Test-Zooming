d0c4fac3d2f4f2a4d8539ddc95abedf26ba18d68
Evan Frenkel <comrad@gmail.com>
2026-02-16 16:17:43 -0800

    Initial Commit

---
+++ Test Zooming/Test_ZoomingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Test_ZoomingApp.swift
+//  Test Zooming
+//
+//  Created by mainuser on 2/16/26.
+//
+
+import SwiftUI
+
+@main
+struct Test_ZoomingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
